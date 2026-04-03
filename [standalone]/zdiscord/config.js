f29c73b218abfcb77d1e470353d8e022181e37b4
DangNhut-dev <dangnhut2908@gmail.com>
2026-04-04 00:27:52 +0700

    Update config.js

--- [standalone]/zdiscord/config.js
+++ [standalone]/zdiscord/config.js
@@ -37,7 +37,7 @@ const AdditionalStaffChatRoleIds = [
 ];
 
 // WHITELISTING / ALLOWLISTING
-const EnableWhitelistChecking = true;
+const EnableWhitelistChecking = false;
 const DiscordWhitelistRoleIds = "1485572756740374620, 000000000000000000";
 
 // SLASH COMMANDS / DISCORD PERMISSIONS
