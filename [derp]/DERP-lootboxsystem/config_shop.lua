46f9299818e3fa636168da4e950520be04be5a57
DangNhut-dev <dangnhut2908@gmail.com>
2026-04-04 02:36:21 +0700

    update đồ nữ lên lootbox

--- [derp]/DERP-lootboxsystem/config_shop.lua
+++ [derp]/DERP-lootboxsystem/config_shop.lua
@@ -42,26 +42,32 @@ Config.Shop.NPCs = {
                 tags  = { 'male' }
             },
             ['lootbox_nam_thuong_matna'] = {
-                id    = 3,
-                label = 'Hòm Mặt Nạ Thường 1 - Nam',
+                id    = 4,
+                label = 'Hòm Mặt Nạ Thường - Nam',
                 price = { cash = 200 },
                 tags  = { 'male' }
             },
             ['lootbox_nu_thuong_aokhoac'] = {
-                id    = 1,
+                id    = 5,
                 label = 'Hòm Áo Thường - Nữ',
                 price = { cash = 200 },
                 tags  = { 'female' }
             },
             ['lootbox_nu_thuong_quan'] = {
-                id    = 2,
+                id    = 6,
                 label = 'Hòm Quần Thường - Nữ',
                 price = { cash = 200 },
                 tags  = { 'female' }
             },
             ['lootbox_nu_thuong_giay'] = {
-                id    = 3,
+                id    = 7,
                 label = 'Hòm Giày Thường - Nữ',
+                price = { cash = 200 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_thuong_matna'] = {
+                id    = 8,
+                label = 'Hòm Mặt Nạ Thường - Nữ',
                 price = { cash = 200 },
                 tags  = { 'female' }
             },
@@ -92,10 +98,45 @@ Config.Shop.NPCs = {
             ['lootbox_nam_balo_s1'] = {
                 id    = 5,
                 label = 'Hòm Balo Đợt 1 - Nam',
-                price = { coin = 5 },
+                price = { coin = 15 },
                 tags  = { 'male' }
             },
-            
+            ['lootbox_nu_aokhoac_s1'] = {
+                id    = 6,
+                label = 'Hòm Áo Đợt 1 - Nữ',
+                price = { coin = 5 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_aotrong_s1'] = {
+                id    = 7,
+                label = 'Hòm Áo Trong Đợt 1 - Nữ',
+                price = { coin = 5 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_balo_s1'] = {
+                id    = 8,
+                label = 'Hòm Balo Đợt 1 - Nữ',
+                price = { coin = 15 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_quan_s1'] = {
+                id    = 9,
+                label = 'Hòm Quần Đợt 1 - Nữ',
+                price = { coin = 5 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_giay_s1'] = {
+                id    = 10,
+                label = 'Hòm Giày Đợt 1 - Nữ',
+                price = { coin = 5 },
+                tags  = { 'female' }
+            },
+            ['lootbox_nu_matna_s1'] = {
+                id    = 11,
+                label = 'Hòm Mặt Nạ Đợt 1 - Nữ',
+                price = { coin = 5 },
+                tags  = { 'female' }
+            },
         }
     }
 }
