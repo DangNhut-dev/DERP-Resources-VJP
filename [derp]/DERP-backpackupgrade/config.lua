53de94f7e63cd1ed07e6b0774a399c23899584c5
DangNhut-dev <dangnhut2908@gmail.com>
2026-04-05 17:49:36 +0700

    Cập nhật lại tỉ lệ balo

--- [derp]/DERP-backpackupgrade/config.lua
+++ [derp]/DERP-backpackupgrade/config.lua
@@ -7,21 +7,21 @@ Config.PropCoords = vector4(194.81, -876.99, 31.27, 66.88)
 -- Điểm cộng theo rarity của item nguyên liệu
 Config.RarityPoints = {
     common    = 10,
-    rare      = 25,
-    epic      = 60,
-    legendary = 150,
-    mythic    = 400,
+    rare      = 50,
+    epic      = 250,
+    legendary = 1250,
+    mythic    = 6250,
 }
 
 -- key = level hiện tại của balo, value = điểm cần để đạt 100% tỉ lệ
 -- Balo ở level không có trong bảng này sẽ không thể nâng cấp (đã max)
 Config.RequirePoints = {
-    [0] = 500,
-    [1] = 1000,
-    [2] = 2000,
+    [0] = 100,
+    [1] = 500,
+    [2] = 2500,
     [3] = 4000,
-    [4] = 8000,
-    [5] = 16000,
+    [4] = 12500,
+    [5] = 15000,
 }
 
 -- Số slot nguyên liệu tối đa mỗi lần nâng cấp
